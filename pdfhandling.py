d63561cec95cf68afab2e406ca104162ac3f6b02
fryymann715 <ideans715@gmail.com>
2016-01-16 10:36:53 -0800

    1/10/16
    Added a few functions to turn the pdf to txt and extract a little bit of data from the text.

--- pdfhandling.py
+++ pdfhandling.py
@@ -2,35 +2,68 @@
 import os
 
 
+comp_type = {0: "Cold Food", 1: "Service", 2: "Time", 3: "Quality", 4: "Didn't Like",
+             5: "Alcohol", 6: "TM Error"}
+
+
+# Pull the text from the pdf file.
 def get_comp_txt():
-    print("Opening comppdf...")
-    compfile = open('pdfminer/comppdf.txt')
-    print("Reading comptxt...")
+    print("Opening File...")
+    compfile = open('pdfminer/paymentdetail.txt')
+    print("Reading File...")
     comptxt = compfile.read()
     compfile.close()
+    print("Closing File...")
     return comptxt
 
 
+# Pull the comps section from the payment details file
 def extract_comps(comptxt):
+    print("Locating comps within file...")
     comp_start = comptxt.find("********   COMPS   ********")
     comptxt = comptxt[comp_start:]
+    print("Comps located...")
     return comptxt
 
 
+# Pulls the specific comp type from the comp text.
 def get_specific_comp(comptxt, comp_type):
+    print("Fetching " + comp_type + " comps...")
     comp_start = comptxt.find("*********  " + comp_type + "   *********")
     comp_end = comptxt.find("Total " + comp_type + ":")
-    comp_50 = comptxt[comp_start:comp_end]
-    print comp_50
+    comps = comptxt[comp_start:comp_end]
+    print("Finished.")
+    return comps
+
+
+# Iterates through the given text and pulls the needed items
+def pull_comps(comptxt):
+    comp_dict = {}
+    for line in comptxt:
 
 
 
+    return comp_dict
 
 
+def get_all_comps(comptxt):
+        counter = 0
+        current_comps = {}
+        while counter < 7:
+            current_comps[comp_type[counter]] = get_specific_comp(comptxt, comp_type[counter])
+            counter += 1
+        return current_comps
+
+payment_detail_text = get_comp_txt()
+comp_all_text = extract_comps(payment_detail_text)
+comp_dict = get_all_comps(comp_all_text)
+
+time_comps = pull_comps(comp_dict[comp_type[2]])
 
 
-test = get_comp_txt()
+print comp_dict[comp_type[2]]
+print " "
+print " "
+print time_comps
 
-test2 = extract_comps(test)
 
-get_specific_comp(test2, "Service")
